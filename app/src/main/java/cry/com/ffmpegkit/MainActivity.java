7407d5deb55f331c4298e41c596dea10ba9729e6
钟朝旭Frank_Android开发 <zhong.zhaoxu@hbtown.com>
2017-08-23 11:05:41 +0800

    修改ffmpeg库

--- app/src/main/java/cry/com/ffmpegkit/MainActivity.java
+++ app/src/main/java/cry/com/ffmpegkit/MainActivity.java
@@ -3,11 +3,14 @@ package cry.com.ffmpegkit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import haibao.com.ffmpegkit.FFmpegKit;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FFmpegKit.initialize(this, "sfhj");
     }
 }
